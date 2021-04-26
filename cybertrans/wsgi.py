af385e7459ee46119bccd9d2c37eb5687e234c2c
Timothy <behuangyi@gmail.com>
2021-04-25 23:43:00 -0700

    updated

--- cybertrans/wsgi.py
+++ cybertrans/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for project_cybertrans project.
+WSGI config for cybertrans project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_cybertrans.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cybertrans.settings')
 
 application = get_wsgi_application()
